bab50009c97b5651cd0872d7030d04b855006b9f
afecego <afecego@gmail.com>
2022-01-12 16:40:02 -0500

    sets up your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Write a Bash script that sets up your web servers for the deployment of web_static
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+echo "It is working" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -s /data/web_static/current /data/web_static/releases/test/
+sudo chown -R ubuntu:ubuntu /data/
+new_string = "server_name _;\n\t\tlocation /hbnb_static/ {\n\t\t\talias /data/web_static/current/;\n\t\t}"
+sudo sed -i "s|server_name\ _;|$new_string|" /etc/nginx/sites-available/default
