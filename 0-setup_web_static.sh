74a5473049e5b1f2a2dc35a923b927d27250e144
afecego <afecego@gmail.com>
2022-01-13 00:06:32 -0500

    Correction flags and echo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,9 +8,16 @@ sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
+echo "
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee -a /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
 new_string="server_name _;\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
 sudo sed -i "s|server_name\ _;|$new_string|" /etc/nginx/sites-available/default
 sudo service nginx restart
