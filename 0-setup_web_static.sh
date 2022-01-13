539477361ac6b130de2070ed1f47765f7e60ad06
afecego <afecego@gmail.com>
2022-01-13 01:21:19 -0500

    chmod

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,8 +15,8 @@ echo "<html>
     Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
 new_string="server_name _;\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
 sudo sed -i "s|server_name\ _;|$new_string|" /etc/nginx/sites-available/default
 sudo service nginx reload
