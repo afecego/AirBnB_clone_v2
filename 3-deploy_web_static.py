a75499c195a3449342a76e4d9d803737bb160145
root <root@LAPTOP-9RRUP922.localdomain>
2022-01-13 22:44:23 -0500

    Deploy web static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -25,6 +25,7 @@ def do_pack():
     except Exception:
         return None
 
+
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
     if exists(archive_path) is False:
