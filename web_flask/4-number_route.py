ca2142cabaf7ed76e00df233c0ed61c92975b274
afecego <afecego@gmail.com>
2022-01-30 22:36:04 -0500

    Add number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello_hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def hello_addtext(text):
+    add = text.replace("_", " ")
+    return 'C {}'.format(add)
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def hello_phytontext(text='is cool'):
+    add = text.replace("_", " ")
+    return 'Python {}'.format(add)
+
+
+@app.route('/number/<int:n>')
+def hello_numbre(n):
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
