448a6606860c991279936d89e501dc01ea30e9cd
afecego <afecego@gmail.com>
2022-01-31 00:13:53 -0500

    Update Storage files

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -31,11 +31,11 @@ class DBStorage():
         HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
         HBNB_ENV = getenv('HBNB_ENV')
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
-                                        format(HBNB_MYSQL_USER,
-                                                HBNB_MYSQL_PWD,
-                                                HBNB_MYSQL_HOST,
-                                                HBNB_MYSQL_DB),
-                                        pool_pre_ping=True)
+                                      format(HBNB_MYSQL_USER,
+                                             HBNB_MYSQL_PWD,
+                                             HBNB_MYSQL_HOST,
+                                             HBNB_MYSQL_DB),
+                                      pool_pre_ping=True)
         if HBNB_ENV == 'test':
             Base.metadata.drop_all(self.__engine)
 
@@ -80,7 +80,3 @@ class DBStorage():
 
     def close(self):
         self.__session.close()
-
-    def close(self):
-        """Call remove method"""
-        self.__session.remove()
