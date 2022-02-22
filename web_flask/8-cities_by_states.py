0f6418c22d947541f4783acc1f89995057e53497
afecego <afecego@gmail.com>
2022-02-21 20:00:39 -0500

    Display data of cities

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,11 +5,10 @@ from models import storage
 from models.state import State
 from models.city import City
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/cities_by_states')
-def states_list():
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_list():
     list_state = storage.all(State)
     return render_template('8-cities_by_states.html', state_all=list_state)
 
@@ -20,4 +19,4 @@ def appcontext(exception):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, strict_slashes=False)
+    app.run(host='0.0.0.0', port=5000)
